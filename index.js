79da5ae44ff9dab8de24d07043c5808d2223ec85
$apucsd <apusutradhar77@gmail.com>
2024-02-16 22:54:53 +0600

    added create supply and get supply route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -26,14 +26,16 @@ async function run() {
     console.log("Connected to MongoDB");
 
     const db = client.db("med-relief-hub");
-    const collection = db.collection("users");
+    const userCollection = db.collection("users");
+    const supplyCollection = db.collection("supplies");
 
     // User Registration
+
     app.post("/api/v1/register", async (req, res) => {
       const { name, email, password } = req.body;
 
       // Check if email already exists
-      const existingUser = await collection.findOne({ email });
+      const existingUser = await userCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({
           success: false,
@@ -45,7 +47,7 @@ async function run() {
       const hashedPassword = await bcrypt.hash(password, 10);
 
       // Insert user into the database
-      await collection.insertOne({ name, email, password: hashedPassword });
+      await userCollection.insertOne({ name, email, password: hashedPassword });
 
       res.status(201).json({
         success: true,
@@ -58,7 +60,7 @@ async function run() {
       const { email, password } = req.body;
 
       // Find user by email
-      const user = await collection.findOne({ email });
+      const user = await userCollection.findOne({ email });
       if (!user) {
         return res.status(401).json({ message: "Invalid email or password" });
       }
@@ -83,6 +85,46 @@ async function run() {
 
     // ==============================================================
     // WRITE YOUR CODE HERE
+
+    app.post("/api/v1/supplies", async (req, res) => {
+      const supply = req.body;
+
+      // Insert user into the database
+      const result = await supplyCollection.insertOne(supply);
+
+      res.status(201).json({
+        success: true,
+        message: "Supply added successfully",
+        result,
+      });
+    });
+    app.get("/api/v1/supplies", async (req, res) => {
+      // find into the database
+      const result = await supplyCollection.find().toArray();
+
+      res.status(201).json({
+        success: true,
+        message: "Supply Post fetched successfully",
+        result,
+      });
+    });
+    app.delete("/api/v1/supplies/:id", async (req, res) => {
+      // find into the database
+      const id = req.params.id;
+      console.log(id);
+      const result = await supplyCollection.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
+
+      console.log(result);
+
+      res.status(201).json({
+        success: true,
+        message: "Supply deleted successfully",
+        result,
+      });
+    });
+
     // ==============================================================
 
     // Start the server
